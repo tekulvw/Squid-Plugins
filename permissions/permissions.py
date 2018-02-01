abe74b4f2cdd3b5d47cf5df28452a8e17e3c11ce
retke <laggron42@gmail.com>
2018-02-01 22:04:48 +0100

    removed spaces

--- permissions/permissions.py
+++ permissions/permissions.py
@@ -777,12 +777,12 @@ class Permissions:
                 raise e
         role = self._get_role(server.roles, role)
         await self._set_permission(command_obj, server, role=role, allow=False)
-        
+
         if role.name.startswith('@'):
             _rolename = role.name[1:]
         else:
             _rolename = role.name
-            
+
         await self.bot.say("Role {} denied use of {}.".format(_rolename,
                                                               command))
 
