c9203edeb3929f970f65f4f856585ee39a7a53d4
aikaterna <20862007+aikaterna@users.noreply.github.com>
2020-08-26 09:23:37 -0700

    phrasing

--- bankvampire/__init__.py
+++ bankvampire/__init__.py
@@ -1,6 +1,6 @@
 from .bankvampire import BankVampire
 
-__red_end_user_data_statement__ = "This cog does store discord IDs as needed for operation."
+__red_end_user_data_statement__ = "This cog stores discord IDs as needed for operation."
 
 
 async def setup(bot):
